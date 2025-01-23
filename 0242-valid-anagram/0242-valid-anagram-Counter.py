ff3de13cf7e322a25c12580bc2ab8277a7709887
Junghwan Bae <136900225+baedori99@users.noreply.github.com>
2025-01-22 18:10:42 -0600

    Time: 21 ms (5.74%), Space: 18.4 MB (12.67%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram-Counter.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        new_s = sorted(s)
+        new_t = sorted(t)
+
+        if len(new_s) == len(new_t):
+            for i in range(len(new_s)):
+                if new_s[i] == new_t[i]:
+                    pass
+                else:
+                    return False
+            return True
+        else:
+            return False
