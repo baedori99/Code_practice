40cbaf5101257c701c9793cba9f9d0fe01083bc0
Junghwan Bae <136900225+baedori99@users.noreply.github.com>
2025-01-28 18:16:44 -0600

    Time: 1208 ms (5.02%), Space: 21.2 MB (64.45%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,12 +1,27 @@
 from collections import Counter
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count_nums = Counter(nums)
+        # count_nums = Counter(nums)
+        # ans = []
+        # sorted_nums = sorted(count_nums.items(), key=lambda x: x[1], reverse = True)
+        
+        # tem_ans = [x[0] for x in sorted_nums]
+        
+        # for i in range(k):
+        #     ans.append(tem_ans[i])
+        # return ans
+        lst = []
+        t = set(nums)
+        sorted_list = list(t)
         ans = []
-        sorted_nums = sorted(count_nums.items(), key=lambda x: x[1], reverse = True)
-        
-        tem_ans = [x[0] for x in sorted_nums]
-        
+
+        for num in t:
+            lst.append(nums.count(num))
+        dict_list = dict(zip(sorted_list,lst))
+        sorted_dict = sorted(dict_list.items(), key=lambda x: x[1], reverse = True)
+
+        temp_sorted = [x[0] for x in sorted_dict]
+
         for i in range(k):
-            ans.append(tem_ans[i])
+            ans.append(temp_sorted[i])
         return ans
