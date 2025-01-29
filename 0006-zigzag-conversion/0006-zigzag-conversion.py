a6d3c63df7726fb36a1807d2fc4f7adfd6e54fb5
Junghwan Bae <136900225+baedori99@users.noreply.github.com>
2025-01-29 13:32:35 -0600

    Time: 11 ms (65.35%), Space: 17.8 MB (56.12%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -1,22 +1,21 @@
+from collections import defaultdict
+
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        ans_list = [[] for _ in range(numRows)]
-
-        idx = 0 # Index
-        d = 1 # Direction
-
         if numRows == 1 or len(s) <= numRows:
             return s
 
+        ans_list = [""] * numRows
+
+        idx = 0
+        d = 1
+
         for char in s:
-            ans_list[idx].append(char)
-            if idx == numRows - 1:
-                d = -1
-            elif idx == 0:
+            ans_list[idx] += char
+            if idx == 0:
                 d = 1
+            elif idx == numRows - 1:
+                d = -1
             idx += d
-
-        for i in range(numRows):
-            ans_list[i] = ''.join(ans_list[i])
-
+        
         return ''.join(ans_list)
